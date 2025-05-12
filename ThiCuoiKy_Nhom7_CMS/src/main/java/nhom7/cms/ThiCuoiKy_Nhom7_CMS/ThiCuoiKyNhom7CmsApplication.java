f0bf5a2730a9e4c58c4a6d3e792eed563859a4ab
KhiemCho <khiem.nhg.64cntt@ntu.edu.vn>
2025-05-12 15:42:10 +0700

    update lan 1

---
+++ ThiCuoiKy_Nhom7_CMS/src/main/java/nhom7/cms/ThiCuoiKy_Nhom7_CMS/ThiCuoiKyNhom7CmsApplication.java
@@ -0,0 +1,13 @@
+package nhom7.cms.ThiCuoiKy_Nhom7_CMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiCuoiKyNhom7CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiCuoiKyNhom7CmsApplication.class, args);
+	}
+
+}
