a38ddcfd46c97a6e4bdd752f6e747e7ee3fd4f80
IamPatricKKK <letruongle325@gmail.com>
2025-05-20 16:04:00 +0700

    Danh muc

--- ThiCuoiKy_Nhom7_CMS/src/main/java/nhom7/cms/ThiCuoiKy_Nhom7_CMS/repository/DanhMucRepository.java
+++ ThiCuoiKy_Nhom7_CMS/src/main/java/nhom7/cms/ThiCuoiKy_Nhom7_CMS/repository/DanhMucRepository.java
@@ -1,7 +1,11 @@
 package nhom7.cms.ThiCuoiKy_Nhom7_CMS.repository;
 
-import nhom7.cms.ThiCuoiKy_Nhom7_CMS.model.DanhMuc;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import nhom7.cms.ThiCuoiKy_Nhom7_CMS.model.DanhMuc;
+
 public interface DanhMucRepository extends JpaRepository<DanhMuc, String> {
+	Page<DanhMuc> findByTenContainingIgnoreCase(String ten, Pageable pageable);
 }
